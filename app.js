bee41aabe7b90e0480a765fbb0087b1bf53336ed
Andrey Yakovenko <yakovenko.a.a@bk.ru>
2015-09-26 16:58:49 +0300

    0.0.5

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var HTTP = require('http');
 var URL = require('url');
 var FS = require('fs');
+var SWIG = require('swig');
+
+var swig = new SWIG.Swig({
+    cache: false,
+    locals: {
+        meta: {
+            title: "xxx"
+        }
+    },
+    loader: SWIG.loaders.fs('./storage', {encoding: 'utf8'})
+});
 
 var srv = HTTP.createServer();
 
@@ -8,7 +19,14 @@ srv.on('request', function (req, res){
     var u = URL.parse(req.url);
     if (u.pathname == '/'){
         res.writeHead(200, {'content-type': 'text/html'});
-        res.end('Frontend');
+        swig.renderFile('templates/default/layout/index-uk.html', {
+            meta: {
+                title: 'yyy'
+            }
+        }, function (err, out){
+            if (err) throw err;
+            res.end(out);
+        });
     }
 
     else {
