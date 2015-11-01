e856026b339578108a31d401b60a6ea4bcce8d7d
Sergej Kukharev <sergej.kucharev@gmail.com>
2015-11-01 17:52:31 +0200

    0.0.15

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ var srv = require('./lib/zcms');
 srv.setup(3001);
 var tpl = require('./lib/tpl');
 
-srv.setCall('GET', /.*/, '/', function (req, res){
+srv.reply('unknown', function(req, res){
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end('unknown');
+}, '*');
+
+srv.reply('frontpage', function(req, res){
     res.writeHead(200, {'content-type': 'text/html'});
     tpl.renderFile('templates/default/layout/index-uk.html', {
     }, function (err, out){
         if (err) throw err;
         res.end(out);
     });
-});
-
-srv.setCall('GET', /.*/, /.*/, function (req, res){
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.end('others');
-});
+}, '/');
