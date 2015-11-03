a375ee28c9c20b0b7ade0f0c09d8acde2b5d1b43
Sergej Kukharev <sergej.kucharev@gmail.com>
2015-11-03 07:28:59 +0200

    0.0.17

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 
-var pf = require('util').format;
-var meta = require('./lib/zcms/usefull').meta;
-var typeOf = require('./lib/zcms/usefull').typeOf;
-
 var srv = require('./lib/zcms');
 srv.setup(3001, 1);
 
