bc111a8647103128863c10eca2ccf2ccc19c0c6a
Sergej Kukharev <sergej.kucharev@gmail.com>
2015-11-08 00:46:35 +0200

    0.0.20

--- test-5.js
+++ test-5.js
@@ -1,5 +1,14 @@
 // mongoDB
 
-var mongoose = require('mongoose');
-console.log(mongoose.version);
+var mongoConnectionString = 'mongodb://localhost:27017/test';
+var mongoClient = require('mongodb').MongoClient;
+
+var i = 11;
+mongoClient.connect(mongoConnectionString, function(err, db){
+	var table = db.collection('t');
+	table.insert([ {a:i++, b:'abc'}, {x:i++}, {y:i++} ], function(error, result){
+		console.log(result);
+		db.close();
+	});
+});
 
