20b5b1610f25a58c1ab1ddb748a4e6cedbc8bb60
Sergej Kukharev <sergej.kucharev@gmail.com>
2015-11-28 16:51:53 +0200

    0.1.2
    session alpha added

--- test-6.js
+++ test-6.js
@@ -1,11 +1,25 @@
+var util = require('util');
+
 var o = {
 	a:11,
 	b:22,
-	c:33,
+	c:{
+		c1:8,
+		c2:9,
+	},
 	d:44
-}
+};
 
-Object.keys(o).forEach(function(index){
-	console.log('k:' + index, 'v:' + o[index]);
+var o2 = {
+	e:55,
+	f:66,
+	d:77,
+	c:{c3:10}
+};
+
+var o3 = Object.assign(o, o2);
+
+Object.keys(o3).forEach(function(index){
+	console.log('k:' + index, 'v:' + o3[index]);
 });
-console.log(o);
+console.log(o3);
